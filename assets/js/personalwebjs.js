a926a81f7f526c88b7b322e6ba23a805378db397
cmm16 <cmm16@rice.edu>
2019-08-07 20:32:10 +0200

    first commit

---
+++ assets/js/personalwebjs.js
@@ -0,0 +1,35 @@
+var currentWidth
+var currentHeight
+
+/*
+$(".card").hover(
+function(){
+    console.log("moused in");
+    currentWidth = $(this).css("width");
+    currentHeight = $(this).css("height");
+    $(this).css({
+        transition : 'background-color 1s ease-in-out',
+        "background-color": 'rgba(0,0,100,.5)'
+    });
+
+},
+function(){
+    console.log("moused out");
+    $(this).css("color", "black");
+    $(this).css({
+        transition : 'background-color 1s ease-in-out',
+        "background-color": "white"
+    });
+})
+
+
+$(function (){
+    $(document).scroll(function() {
+        var $intro = $("#intro")
+        $(".navbar").toggleClass("scrolled", $(this).scrollTop() > $("#intro").height());
+        console.log("nav height " + $("#intro").height())
+        console.log("doc height " + $(this).scrollTop());
+    });
+    
+});
+*/
